a5af9b82dc5f04a62ce58d6dde519ddf08b8c755
Anand jha <codewithann@gmail.com>
2023-12-31 23:47:04 +0530

    use splashScreen

--- lib/splashScreen.dart
+++ lib/splashScreen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:share_preferance/loginScreen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -8,6 +11,16 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => const LoginScreen()));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
